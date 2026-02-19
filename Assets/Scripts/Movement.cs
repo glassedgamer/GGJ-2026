8397dcc1e7f4a1128f77ae7432dabdb78f25ff67
glassedgamer <glassedgamer05@gmail.com>
2026-02-19 10:58:51 -0500

    New Input System
    Implemented Unity's new input system so I could add controller support (which works beautifully)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,19 +9,24 @@ public class Movement : MonoBehaviour
 
     [SerializeField] GameObject bullet;
 
-    [SerializeField] KeyCode shoot;
+    //[SerializeField] KeyCode shoot;
+
+    Vector2 lookInput;
 
     public float planeSpeed;
 
-    public float sensX = 2f;
-    public float sensY = 1f;
+    public float mouseSens = 2f;
+    public float stickSens = 120f;
+    //public float sensY = 1f;
     public float yClamp;
 
     public float tilt = 30f;
 
     float xRot;
     float yRot;
-    
+
+    bool usingGamepad;
+
     void Start()
     {
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
@@ -33,13 +38,15 @@ public class Movement : MonoBehaviour
         CameraMovement();
         transform.Translate(Vector3.forward * planeSpeed * Time.deltaTime);
 
-        Shooting();
+        //Shooting();
     }
 
     void CameraMovement()
     {
-        float inputX = Input.GetAxis("Mouse X") * sensX;
-        float inputY = Input.GetAxis("Mouse Y") * sensY;
+        float sens = usingGamepad ? stickSens : mouseSens;
+
+        float inputX = lookInput.x * sens * Time.deltaTime;
+        float inputY = lookInput.y * -sens * Time.deltaTime;
 
         yRot += inputX;
 
@@ -60,11 +67,25 @@ public class Movement : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
     }
 
+    /*
     void Shooting()
     {
         if(Input.GetKeyDown(shoot))
         {
             Instantiate(bullet, shootPoint.position, this.transform.rotation);
         }
+    }
+    */
+
+    // Grabs Vector2 input from context and also checks if you're using a controller or not
+    public void OnLook(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        usingGamepad = context.control.device is UnityEngine.InputSystem.Gamepad;
+    }
+
+    public void OnShoot(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        Instantiate(bullet, shootPoint.position, this.transform.rotation);
     }
 }
