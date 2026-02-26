61855dff555b0c37ad8b9be81c0edbc11e85f3df
glassedgamer <glassedgamer05@gmail.com>
2026-02-26 10:47:05 -0500

    Audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,5 +3,20 @@ using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one audio manager in the scene dummy");
+        }
+
+        instance = this;
+    }
+
+    public void PlayerOneShot(EventReference sound, Vector3 worldSpace)
+    {
+        RuntimeManager.PlayOneShot(sound, worldSpace);
+    }
 }
