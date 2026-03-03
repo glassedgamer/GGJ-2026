7b9823a0893057d13118f5c542401a306cd77744
masonm224 <masonm224@icloud.com>
2026-03-03 01:26:56 -0500

    Annoyed
    So close to getting the timeline to trigger on the box. Just missing something

---
+++ Assets/Scripts/Managers/Level3Manager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Level3Manager : MonoBehaviour
+{
+        //Animator anim;
+
+    void Start()
+    {
+        //anim = gameObject.GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+        /*if(collision.gameObject.tag == "TriggerDoor")
+        {
+            //anim.SetTrigger("DoorsCloseTrigger");
+        }*/
+    }
+}
--- Assets/Scripts/Player/PlayerCollisions.cs
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
-
+    Animator anim;
     GameManager gameManager;
+
+    [SerializeField] private string boolParameterName = "DoorsClose";
 
     private void Start()
     {
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -15,6 +18,14 @@ public class PlayerCollisions : MonoBehaviour
         if(collision.gameObject.tag == "Death" || collision.gameObject.tag == "Death Enemy")
         {
             gameManager.BackToMain();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(gameObject.tag == "TriggerDoor")
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Animator.SetBool(DoorsClose, true);
         }
     }
 }
